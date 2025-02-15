dcdfc24e3452e555159f4cca355ab6d39e47e1c0
Shawon Kumar Sarkar <shawonkumarsarkar.bd@gmail.com>
2025-02-16 00:04:42 +0600

    Setup complete for Biker-Bazaar-Client

---
+++ src/MainLayout/MainLayout.tsx
@@ -0,0 +1,12 @@
+import Home from "../Pages/Home/Home";
+
+
+const MainLayout = () => {
+    return (
+        <div className="font-poppins w-full min-h-screen overflow-x-hidden">
+            <Home />
+        </div>
+    );
+};
+
+export default MainLayout;
